91fdb060dfb1a16bac5dbf8feaf59b2906360fa3
ahmedammar619 <amgammarx@hotmail.com>
2025-12-05 04:51:54 -0600

    X

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: 'all',
+    allowedHosts: true,
     watch: {
       usePolling: true,
       interval: 1000
